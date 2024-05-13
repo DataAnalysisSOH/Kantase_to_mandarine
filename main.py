3939b1553df3ae0ad0165a9b9cd7b8b042115c23
Lucy <yuqianlucy@gmail.com>
2024-05-13 17:01:38 -0400

    create function text to string to turn articles into string

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import csv
-
 # We are define an function to read csv file and turn into dictionary
 def read_csv_to_dict(csv_file):
     # create a empty dictionary to store
@@ -14,6 +13,17 @@ def read_csv_to_dict(csv_file):
             cantonese = row['Cantonese']
             mandarin_cantonese_dict[mandarin] = cantonese
         return mandarin_cantonese_dict
+    
+# We are creating a function to turn text into strings
+def read_txt_to_string(txt_file):
+    # We first need an empty string
+    articles = ""
+    with open(txt_file, 'r', encoding='utf-8') as file:
+        # We are read each line in file
+        for line in file:
+            articles += line
+    # We are Return the concatenated string containing the contents of the file
+    return articles
 # We are define the main method
 def main():
     # We are defining the csv_file path
